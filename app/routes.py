ef51bed15e148ced9a4dae8abd45fee33a993e45
Nitish <nitin12345chaudhary@gmail.com>
2025-06-29 17:21:31 +0530

    second module

---
+++ app/routes.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import os
+import json
+from werkzeug.utils import secure_filename
+from app.utils import hash_password
+
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+DATA_FILE = 'file_metadata.json'
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files or 'password' not in request.form:
+        return jsonify({'error': 'Missing file or password'}), 400
+
+    file = request.files['file']
+    password = request.form['password']
+    filename = secure_filename(file.filename)
+
+    filepath = os.path.join(UPLOAD_FOLDER, filename)
+    file.save(filepath)
+
+    hashed_pw = hash_password(password)
+
+    # Save metadata to JSON (temporary storage)
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r') as f:
+            data = json.load(f)
+    else:
+        data = {}
+
+    data[filename] = hashed_pw
+
+    with open(DATA_FILE, 'w') as f:
+        json.dump(data, f)
+
+    return jsonify({'message': f'File {filename} uploaded successfully'})
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from app.utils import hash_password , verify_password
+from app.routes import app
 
-hashed = hash_password(b"mypassword")
+hashed = hash_password("mypassword")
 print(hashed)
 print("verfied : ", verify_password("mypassword" , hashed))
 print("wrong : ", verify_password("wrongpass" , hashed))
+
+if __name__ == "__main__":
+    app.run(debug=True)
